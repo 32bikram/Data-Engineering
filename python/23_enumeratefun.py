df16f190e8a6c5155873d23a20afe85e5fcc333e
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-07-30 21:19:16 +0530

    Fix enumerate loop to display index and value
    Refactor enumerate loop to print both index and value.

--- python/23_enumeratefun.py
+++ python/23_enumeratefun.py
@@ -1,10 +1,13 @@
 ml = [9,8,7,6,5,4,3,2,1]
 for i in ml:
     print(i)
+#op - 987.....
 
 for i in enumerate(ml):
     print(i)
+#op- (0,9) (1,8).....like dict
 
 for i,j in enumerate(ml):
     print(i)
     print(j)
+#op - 0 9 1 8 2 7......i is 0123..... j is 9876.....
