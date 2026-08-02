271cf368995942bca76e0c9d6fdaf90f787248c3
32bikram <bikramsarkareveland@gmail.com>
2026-08-02 15:11:11 +0000

    ewjnhfxihwfhoo

---
+++ python/Codechef_Practice/02_comma_in_print.py
@@ -0,0 +1,6 @@
+print("Code","Chef")
+
+#Correct Answer:
+#Code Chef
+#Explanation:
+#Multiple outputs are joined together using the "," symbol and they use " " as the delimeter.
---
+++ python/Codechef_Practice/03_skip_newline.py
@@ -0,0 +1,9 @@
+print(5)
+print(7, end = " ")
+print(9)
+# Correct Answer:
+# Prints one number on the first line, two numbers on the next line
+#5
+#79
+# Explanation:
+# end=" " is used to not skip to a new line
